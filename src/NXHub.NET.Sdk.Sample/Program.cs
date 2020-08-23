6ee26f636c2faaf7e851bb0abfa8ef02c9fee406
codeping <deping.du@gmail.com>
2020-08-23 21:31:23 +0800

    添加示例项目。

---
+++ src/NXHub.NET.Sdk.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NXHub.NET.Sdk.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
